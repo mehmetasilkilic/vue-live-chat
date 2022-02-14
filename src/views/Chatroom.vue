ed9cc201445bc676e543614c34a262ce2bfa4de3
mehmetasilkilic <87938113+mehmetasilkilic@users.noreply.github.com>
2022-02-14 13:42:26 +0300

    navbar

---
+++ src/views/Chatroom.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <Navbar />
+    <NewChatForm />
+  </div>
+</template>
+
+<script>
+import NewChatForm from "../components/NewChatForm.vue";
+import { watch } from "vue";
+import Navbar from "../components/Navbar.vue";
+import getUser from "../composables/getUser";
+import { useRouter } from "vue-router";
+export default {
+  components: { Navbar, NewChatForm },
+  setup() {
+    const { user } = getUser();
+    const router = useRouter();
+
+    watch(user, () => {
+      if (!user.value) {
+        router.push({ name: "Welcome" });
+      }
+    });
+  },
+};
+</script>
+
+<style>
+</style>
